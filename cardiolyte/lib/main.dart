f5afa2ce83a014673d51a5a9a6a779b4cc8fe8d7
kitkashi <skit.edwards@gmail.com>
2026-04-26 18:57:42 -0500

    working on updating the data source for graph

--- cardiolyte/lib/main.dart
+++ cardiolyte/lib/main.dart
@@ -1,5 +1,18 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+
+//instantiate random number
+final Random random = Random();
+ChartSeriesController? _chartSeriesController;
+List<EkgSampleData> ekgData = <EkgSampleData>[
+  EkgSampleData(x: 1, y: 30),
+  EkgSampleData(x: 3, y: 13),
+  EkgSampleData(x: 5, y: 80),
+  EkgSampleData(x: 7, y: 30),
+  EkgSampleData(x: 9, y: 72)
+];
 
 void main() {
   return runApp(_ChartApp());
@@ -15,6 +28,8 @@ class _ChartApp extends StatelessWidget {
   }
 }
 
+
+  
 class _MyHomePage extends StatefulWidget {
   // ignore: prefer_const_constructors_in_immutables
   _MyHomePage({Key? key}) : super(key: key);
@@ -24,13 +39,6 @@ class _MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<_MyHomePage> {
 
-List<EkgSampleData> ekgData = <EkgSampleData>[
-  EkgSampleData(x: 1, y: 30),
-  EkgSampleData(x: 3, y: 13),
-  EkgSampleData(x: 5, y: 80),
-  EkgSampleData(x: 7, y: 30),
-  EkgSampleData(x: 9, y: 72)
-];
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +55,7 @@ List<EkgSampleData> ekgData = <EkgSampleData>[
             // tooltipBehavior: TooltipBehavior(enable: true),
             series: <CartesianSeries<EkgSampleData, num>>[
               LineSeries<EkgSampleData, num>(
-                dataSource: ekgData,
+                dataSource: ekgData, 
                 xValueMapper: (EkgSampleData ekgData, _) => ekgData.x,
                 yValueMapper: (EkgSampleData ekgData, _) => ekgData.y,
                 // name: 'Reading',
@@ -60,12 +68,41 @@ List<EkgSampleData> ekgData = <EkgSampleData>[
       ),
     );
   }
-}
+}                        
 
 
 class EkgSampleData {
   EkgSampleData({this.x, this.y});
 
-  num? x;
-  num? y;
+  int? x;
+  int? y;
 }
+
+int _getRandomInt(int min, int max) {
+  return min + random.nextInt(max - min);
+}
+
+/// Continuously updating the data source based on timer.
+void _updateDataSource(Timer timer) {
+  int i=0;
+  ekgData.add(EkgSampleData(x: i++, y: _getRandomInt(10, 100)));
+  if (ekgData.length == 20) {
+    ekgData.removeAt(0);
+    _chartSeriesController?.updateDataSource(
+      addedDataIndexes: <int>[ekgData.length - 1],
+      removedDataIndexes: <int>[0],
+    );
+  } else {
+    _chartSeriesController?.updateDataSource(
+      addedDataIndexes: <int>[ekgData.length - 1],
+    );
+  }
+}                         
+
+Timer? timer;
+
+@override
+void initState() {
+  super.initState();
+  timer = Timer.periodic(const Duration(milliseconds: 100), _updateDataSource);
+}                          
